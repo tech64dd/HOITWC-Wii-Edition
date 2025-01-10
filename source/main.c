b0a13ea45ed0b65c06d2f1b01535018a6abbc086
tech64dd <tech64@vivaldi.net>
2025-01-11 00:31:41 +0100

    wii edition

--- source/main.c
+++ source/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <gccore.h>
 #include <time.h>
+#include <wiiuse/wpad.h>
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
 
 static char *months[] = {
 	"????",
@@ -79,12 +85,54 @@ static int dateDifference(int d1, int m1, int y1, int d2, int m2, int y2,
 	return totalDaysBetweenDates(d1, m1, y1, d2, m2, y2);
 }
 
+//---------------------------------------------------------------------------------
+int main(int argc, char **argv) {
+//---------------------------------------------------------------------------------
+
+	// Initialise the video system
+	VIDEO_Init();
+
+	// This function initialises the attached controllers
+	WPAD_Init();
+
+	// Obtain the preferred video mode from the system
+	// This will correspond to the settings in the Wii menu
+	rmode = VIDEO_GetPreferredMode(NULL);
+
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+	//SYS_STDIO_Report(true);
+
+	// Set up the video registers with the chosen mode
+	VIDEO_Configure(rmode);
+
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+
+	// Make the display visible
+	VIDEO_SetBlack(false);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
+	VIDEO_WaitVSync();
+	if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
 
 
-int main() {
+	// The console understands VT terminal escape codes
+	// This positions the cursor on row 2, column 0
+	// we can use variables for this with format codes too
+	// e.g. printf ("\x1b[%d;%dH", row, column );
+	printf("\x1b[2;0H");
+
+
 	int numTotalDays, numYears, numMonths, numDays;
 	int day, month, year;
-	puts("The Wii uses the PowerPC 750CL CPU, a minor iteration on the PowerPC 750 design.\nThis CPU is also known as the \"G3\" from Apple.");
+	puts("The Wii uses the PowerPC 750CL CPU, a minor iteration on the PowerPC 750\ndesign.\nThis CPU is also known as the \"G3\" from Apple.");
 	printf("The PowerPC 750 released on %s %d, %d.\n", months[relMon], relDay, relYear);
 
 	time_t t = time(NULL);
@@ -96,7 +144,7 @@ int main() {
 
 	numTotalDays = dateDifference(relDay, relMon, relYear, day, month, year, &numDays, &numMonths, &numYears);
 	
-	printf("Today is: %s %d, %d.  It has been %d days, or %d years, %d months, and %d days, since the release of the PowerPC 750.\n",
+	printf("Today is: %s %d, %d.  It has been %d days, or %d years, %d months,\nand %d days, since the release of the PowerPC 750.\n",
 			months[month],
 			day,
 			year,
@@ -105,4 +153,22 @@ int main() {
 			numMonths,
 			numDays
 	      );
+
+	while(1) {
+
+		// Call WPAD_ScanPads each loop, this reads the latest controller states
+		WPAD_ScanPads();
+
+		// WPAD_ButtonsDown tells us which buttons were pressed in this loop
+		// this is a "one shot" state which will not fire again until the button has been released
+		u32 pressed = WPAD_ButtonsDown(0);
+
+		// We return to the launcher application via exit
+		if ( pressed & WPAD_BUTTON_HOME ) exit(0);
+
+		// Wait for the next frame
+		VIDEO_WaitVSync();
+	}
+
+	return 0;
 }
